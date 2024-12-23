4ac35093c66ba3d16920224dbeefe1a4f3839753
Barthelemy Bonhomme <bbonhomme@ea.com>
2024-12-23 08:13:14 +0100

    chore: setup repro

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -5,6 +5,16 @@ export const Media: CollectionConfig = {
   access: {
     read: () => true,
   },
+  hooks: {
+    afterRead: [
+      ({ doc }) => {
+        return {
+          ...doc,
+          thumbnailURL: `/_next/image?url=${doc.url}&w=384&q=75`
+        }
+      },
+    ],
+  },
   fields: [
     {
       name: 'alt',
